ec9b328fcdcc2696578372b7127cf928f4388375
Rhinkal <augusto.antunes@hotmail.com>
2019-11-02 00:36:48 -0300

    Organizado projeto, categorizando as pastas de inimigos e boss ranged. Feita animação de Boss e Inimigos Ranged, com Idle, Mirando e Atirando. Criado Script para tiro e Mirar no player básico. Configurado maquina de estados para Inimigo e Boss Ranged. Prefabs de Projetil(Mao), Inimgio e Boss Ranged

---
+++ 2DPackageCreator/Assets/Ranged/Scripts/Ranged_Tiro.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ranged_Tiro : MonoBehaviour
+{
+    public Rigidbody2D projetil;
+
+    public Transform emissor;
+    public Transform braco;
+
+    public Transform player;
+
+    private void Start()
+    {
+        braco = transform.GetChild(0);
+    }
+
+    private void Update()
+    {
+        Vector2 p_Pos = new Vector3(player.transform.position.x, player.transform.position.y);
+        braco.transform.right = p_Pos * -1;
+
+    }
+
+    public void Atira ()
+    {
+        Rigidbody2D mao = Instantiate(projetil, emissor.position, emissor.rotation);
+        mao.AddForce(Vector2.left * 1000);
+        Object.Destroy(mao, 2.0f);
+    }
+
+}
